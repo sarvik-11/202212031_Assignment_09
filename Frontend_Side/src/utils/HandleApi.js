64eb88648d9e28fab92ee17702653bdc27882d6d
Sarvik-vag <sarvikvaghasiya611@gmail.com>
2023-05-15 11:23:52 +0530

    Final_1

--- Frontend_Side/src/utils/HandleApi.js
+++ Frontend_Side/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "http://localhost:8000";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
